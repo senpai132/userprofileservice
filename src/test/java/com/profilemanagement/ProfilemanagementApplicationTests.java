6cffc3a227d4ce15d373d5094b3dd95c2876e799
senpai132 <acanikolic021@gmail.com>
2022-09-19 19:15:19 +0200

    feat: added tests

--- src/test/java/com/profilemanagement/ProfilemanagementApplicationTests.java
+++ src/test/java/com/profilemanagement/ProfilemanagementApplicationTests.java
@@ -1,13 +1,31 @@
 package com.profilemanagement;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.profilemanagement.model.UserProfile;
+import com.profilemanagement.repositories.UserProfileRepository;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class ProfilemanagementApplicationTests {
 
+	@Autowired
+	private UserProfileRepository userRepository;
+
 	@Test
-	void contextLoads() {
+	void testGetUserByUsername() {
+		UserProfile u = userRepository.findByUsername("uki");
+		assertThat(u).isNotNull();
 	}
 
+	@Test
+	void testGetAllUsers() {
+		List<UserProfile> users = userRepository.findAll();
+		assertThat(users.size()).isGreaterThan(0);
+	}
 }
