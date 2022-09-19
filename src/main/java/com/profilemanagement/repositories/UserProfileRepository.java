8626e5815638c3b37869d1ba857a4d4575b0929d
senpai132 <acanikolic021@gmail.com>
2022-09-19 20:59:56 +0200

    feat: 13.1. added aux requests

--- src/main/java/com/profilemanagement/repositories/UserProfileRepository.java
+++ src/main/java/com/profilemanagement/repositories/UserProfileRepository.java
@@ -1,5 +1,7 @@
 package com.profilemanagement.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.profilemanagement.model.UserProfile;
@@ -7,4 +9,5 @@ import com.profilemanagement.model.UserProfile;
 public interface UserProfileRepository extends JpaRepository<UserProfile, String>{
     UserProfile findByUsername(String username);
     UserProfile findById(Integer id);
+    List<UserProfile> findByVisibility(int visibility);
 }
